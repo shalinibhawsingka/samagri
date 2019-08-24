11e3b560b73fcb1a48256f8095f769bb19cf07c9
shalinibhawsingka <shalinibhawsingk@gmail.com>
2019-08-24 17:57:05 +0530

    Dependency destroy Added

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,7 @@ class Item < ApplicationRecord
   belongs_to :employee, optional: true
 
   has_many :posts, dependent: :destroy
-  has_many :storage
-  has_many :issues
+  has_many :issues, dependent: :destroy
 
   validates :name, presence: { message: "must be given please" }
   validates :name, uniqueness: { message: "has been taken already" }
